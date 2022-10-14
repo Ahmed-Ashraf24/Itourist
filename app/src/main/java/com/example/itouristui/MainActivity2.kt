2b8d29186d83ac73ebca1ee752214c52c42cee67
Mohamed <stanxiv1@gmail.com>
2022-10-14 13:43:30 +0200

    test

--- app/src/main/java/com/example/itouristui/MainActivity2.kt
+++ app/src/main/java/com/example/itouristui/MainActivity2.kt
@@ -5,14 +5,18 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.RadioButton
 import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main2.*
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        MainAct3NavView.setNavigationItemSelectedListener(this)
+        MainAct3NavView.itemIconTintList = null
 
         var checkedRadioButton = RadioGroupMainOptionsView.checkedRadioButtonId
         RadioGroupMainOptionsView.setOnCheckedChangeListener { radioGroup, i ->
@@ -28,7 +32,13 @@ class MainActivity2 : AppCompatActivity() {
                 ProfileRadioButton.id->ProfileRadioButton.text = "Profile"
                 ChatRadioButton.id->ChatRadioButton.text = "Chat"
             }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
 
         }
+        return true
     }
 }
