7663f522313c88074226d2902d9d68b67e6b2cf2
Mohamed <stanxiv1@gmail.com>
2022-10-16 21:55:32 +0200

    After Merging

--- app/src/main/java/com/example/itouristui/HomeFragment.kt
+++ app/src/main/java/com/example/itouristui/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.itouristui
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.UnderlineSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -20,5 +25,22 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        with(RVsuggestedPlacesId){
+            layoutManager = LinearLayoutManager(requireContext() , LinearLayoutManager.HORIZONTAL , false)
+            adapter = horizontal_recyclerview_adapter()
+        }
+
+        val ss: SpannableString = SpannableString("See More")
+        val underLineSpan = UnderlineSpan()
+
+        ss.setSpan(underLineSpan, 0, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tVId5.text = ss
+    }
+
 }
