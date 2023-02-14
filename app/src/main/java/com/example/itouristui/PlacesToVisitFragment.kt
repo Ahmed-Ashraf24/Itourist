cde363c64976e15b1ff373eacccca38a45c345dd
Mohamed <stanxiv1@gmail.com>
2023-02-14 20:10:06 +0200

    Before Merging Profile VIew Pager

--- app/src/main/java/com/example/itouristui/PlacesToVisitFragment.kt
+++ app/src/main/java/com/example/itouristui/PlacesToVisitFragment.kt
@@ -5,24 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_profile_v2.*
 
 
-class ProfileFragment : Fragment() {
+class PlacesToVisitFragment : Fragment() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_v2, container, false)
+        return inflater.inflate(R.layout.fragment_places_to_visit, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        VpId.adapter = ViewPagerAdapter(requireActivity())
-    }
 
 }
