c427f8e4558040614e7da9a25e448798dfc19b90
Mohamed <cshaco8@gmail.com>
2023-07-04 12:24:50 +0300

    Form Xml

--- app/src/main/java/com/example/itouristui/UI/Tours/RequestTourFragment.kt
+++ app/src/main/java/com/example/itouristui/UI/Tours/RequestTourFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.appyvet.materialrangebar.RangeBar
 import com.bumptech.glide.Glide
 import com.example.itouristui.R
+import com.example.itouristui.UI.Dialogs.DatePickerDialog
 import kotlinx.android.synthetic.main.fragment_request_tour.*
 
 class RequestTourFragment : Fragment() {
@@ -32,12 +34,37 @@ class RequestTourFragment : Fragment() {
 
         FormBudgetRangeBar.setOnRangeBarChangeListener(object : RangeBar.OnRangeBarChangeListener {
             override fun onRangeChangeListener(rangeBar: RangeBar?, leftPinIndex: Int, rightPinIndex: Int, leftPinValue: String?, rightPinValue: String?) {
-               FormRangeOfBudgetEditText.setText("$leftPinValue - $rightPinValue")
+               RangeOfBudgetEditText.setText("$leftPinValue - $rightPinValue")
             }
 
             override fun onTouchStarted(rangeBar: RangeBar?) {}
 
             override fun onTouchEnded(rangeBar: RangeBar?) {  }
         })
+
+        MaleCardView.setOnClickListener { AccompaniedByEditText.setText("Male") }
+        FemaleCardView.setOnClickListener { AccompaniedByEditText.setText("Female") }
+
+        CarPreferredCardView.setOnClickListener { CarPreferenceEditText.setText("Yes") }
+        CarNotPreferredCardView.setOnClickListener { CarPreferenceEditText.setText("No") }
+
+        AddDateOfArrivalImageView.setOnClickListener{
+            DatePickerDialog{
+                DateOfArrivalEditText.setText("${it[0]}/${it[1]}/${it[2]}")
+            }.show(childFragmentManager,"DIALOG_OF_REQUEST_DATE")
+        }
+
+        SubmitRequestButton.setOnClickListener {
+            if (validatePrimaryFields()){
+
+            }else Toast.makeText(requireContext(),"All Primary Fields Must Be Filled",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun validatePrimaryFields() :Boolean{
+        return (IndividualCountEditText.text?:"").toString().isNotBlank()
+                &&(DateOfArrivalEditText.text?:"").toString().isNotBlank()
+                &&(RangeOfBudgetEditText.text?:"").toString().isNotBlank()
+                &&(DescriptionEditText.text?:"").toString().isNotBlank()
     }
 }
